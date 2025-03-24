2c9befa13a1c719b7ad2eea51fbc53ed6eace046
Jonathan Hernandez <jonathanhernandez@Laptop-de-Jonathan.local>
2025-03-24 18:40:39 -0500

    Deslogueo cuando cambie token

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { initializeAuthListener } from '@/stores/authStore'
+import { initializeAuthListener, useAuthStore } from '@/stores/authStore'
 
 import App from './App.vue'
 import router from './router'
@@ -13,5 +13,18 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 initializeAuthListener()
+
+// ⏱️ Listener que detecta cambios en el token y desloguea al instante
+window.addEventListener('storage', (event) => {
+  if (event.key === 'token') {
+    const authStore = useAuthStore()
+
+    const nuevoToken = localStorage.getItem('token')
+    if (!nuevoToken || nuevoToken !== authStore.token) {
+      authStore.logout()
+      router.push('/login')
+    }
+  }
+})
 
 app.mount('#app')
